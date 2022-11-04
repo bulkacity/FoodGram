5f43999cfe4598e143eaf8b883072f4a051d5027
bulkacity <112503952+bulkacity@users.noreply.github.com>
2022-11-04 01:43:17 -0400

    Added the code for selecting the picture id and adding to the attribute of the img sources

--- assets/frameWork.js
+++ assets/frameWork.js
@@ -25,3 +25,115 @@ After document load, run function(findLocalRestaurants) to search through Google
 
 
 */
+let pos;
+let map;
+let bounds;
+let infoWindow;
+let currentInfoWindow;
+let service;
+let infoPane;
+var placeIdExtracted=[];
+var imgLinks = document.querySelectorAll('img');
+console.log (imgLinks.length);
+counterPic=0;
+function initMap(){
+
+    
+    bounds = new google.maps.LatLngBounds();
+    infoWindow = new google.maps.InfoWindow;
+    currentInfoWindow = infoWindow;
+
+    // Geolocation
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };   
+               
+            // Call Places Nearby Search on user's location
+            LocalSearch((pos.lat),(pos.lng))
+
+        },() => {
+            // Browser supports geolocation, but user has denied permission
+            handleLocationError(true, infoWindow);
+          });
+        } else {
+          // Browser doesn't support geolocation
+          handleLocationError(false, infoWindow);
+    
+        }
+    
+        
+}
+
+function LocalSearch(latInput,lngInput) {
+    var pyrmont = new google.maps.LatLng(latInput,lngInput);
+  
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+      });
+  
+    var request = {
+      location: pyrmont,
+      radius: '2500',
+      keyword: 'Restaurant'
+    };
+  
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+   
+    
+  }
+  
+  function callback(results, status) {
+    console.log(results[1].place_id)
+    console.log(results)
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        placeIdExtracted[i] = results[i].place_id;
+        
+      }
+      
+    }
+    console.log("The IDs are here" + placeIdExtracted + "the")
+    
+
+        for( var i=0;i<=placeIdExtracted.length;i++) {
+            searchPicWithID(placeIdExtracted[i])
+            console.log("testing"+[i]);
+
+  }
+}
+
+  function searchPicWithID(arrayPlaceID){
+    //defining the return of picture elements
+    
+    var requestPic = {
+        placeId: arrayPlaceID,
+        fields: ['photos'] //,'name', 'rating', 'formatted_phone_number'
+      };
+      service = new google.maps.places.PlacesService(map);
+     service.getDetails(requestPic, callback);
+      // goal here is to input a function that will take the (place[i].photo[1].getUrl and add), which is the result of the photo attributes and then generate a class object into the html 
+      
+function callback(place, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    
+    console.log(place);
+    let firstPhoto = place.photos[1];
+    let photo=document.createElement('img');
+    photo.classList.add('hero');
+    photo.src = firstPhoto.getUrl();
+    console.log("Selecting image source"+ photo.src);
+    if(!counterPic<imgLinks.length){
+    imgLinks[counterPic].setAttribute("src",photo.src)
+    counterPic++;
+  }
+}
+   
+               
+}
+  }
+//   service.getDetails(request, callback);
