87e8a5f7af3fd233d4e3a577b68842cbe1459bc4
bulkacity <112503952+bulkacity@users.noreply.github.com>
2022-11-03 20:09:22 -0400

    Donot approve this

---
+++ assets/frameWork.js
@@ -0,0 +1,27 @@
+/*
+
+Open application 
+
+Upon opening webpage, function() of google geolocation API is ran automatically.
+
+After document load, run function(findLocalRestaurants) to search through Google Maps for restaurants.(function will search for max of 9 restaurants(limit the query) with in a 2500 meter radius)
+    
+    findLocalRestaurants function loops for a total of 9 iterations. This will return an array of objects.
+    
+    return Array of objects containing name,address,number,photos 
+
+    function loadPhotos() this function will load photos
+        Create attributes of images utilizing JQUERY to set attributes to divs in the main HTML document
+        --> Module 5 ex. 07 Module 4 ex 06
+
+        create HTML src attribute with JQUERY to add an HTML and link to the photo
+            Requires function to have an event handler
+            
+    add function for API 2 , currently options are play random sound upon picture clicks 
+        --> Module 5 ex 03 and module 04 ex 12 and module 4 ex 19
+
+
+
+
+
+*/
